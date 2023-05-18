11f3df827d1079319903ee17c5505eb0c54bcca8
laynester <laynebalsters@gmail.com>
2023-05-18 11:22:57 -0400

    add saving back

--- src/hooks/useApp.ts
+++ src/hooks/useApp.ts
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useBetween } from 'use-between';
-import { useGame } from './game';
+import { useGame, useSaveGame } from './game';
+import { useFloors } from './useFloors';
 
 const state = () => {
     const [ready, setReady] = useState<boolean>(false);
     const [standalone, setAlone] = useState(false);
     const {} = useGame();
-    // const {} = useSaveGame();
-    // const {} = useFloors();
+    const {} = useSaveGame();
+    const {} = useFloors();
 
     useEffect(() => {
         let displayMode = 'browser';
